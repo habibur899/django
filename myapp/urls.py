87303295a4663f42046da59920717622f1ac2806
Habibur Rahaman <habiburg3@gmail.com>
2025-10-08 12:58:19 +0600

    Template added

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,6 @@ from myapp import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
-    path('dashboard/', views.dashboard, name='dashboard')
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('about/', views.about, name='about')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
 # Create your views here.
 def home(request):
     return HttpResponse('Hello World!')
@@ -13,3 +14,6 @@ def profile(request):
 
 def dashboard(request):
     return HttpResponse('This is the dashboard page')
+
+def about(request):
+    return render(request, 'about.html')
