32d284d401c14222771f548bf2c4d8c7e317c943
Habibur Rahaman <habiburg3@gmail.com>
2025-10-10 06:38:52 +0600

    Template Tag If else and for loop

--- myapp/views.py
+++ myapp/views.py
@@ -9,35 +9,41 @@ def home(request):
 
 
 def profile(request):
-    return HttpResponse('This is the profile page')
+    data = {
+        "user": {"is_authenticated": True, "username": "Habibur"},
+        "title": "The quick brown fox jumps over the lazy dog",
+        "products": [{"name": "Samsung Galaxy S21", "price": 75000},
+                    {"name": "Apple iPhone 14 Pro Max", "price": 80000},
+                    {"name": "Samsung Galaxy S22", "price": 90000}, ]
+    }
+
+    return render(request, 'profile.html', data)
 
 
 def dashboard(request):
     return HttpResponse('This is the dashboard page')
 
 
-data = {
-    "title": "The quick brown fox jumps over the lazy dog",
-    "name": "habibur",
-    "word": "Django Templates Contex Feature",
-    "sentence": "This is a simple Django Template",
-    "number": "1234567890",
-    "list": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    "my_dict": {"name": "habibur", "age": 25, "city": "Lahore"},
-    "my_html": "<h1><i>Hello World!</i></h1>",
-    "my_date": datetime.now() - timedelta(days=365),
-    "my_string": "alert('hello');",
-    "my_var": None,
-    "is_bangladeshi": True,
-
-    "my_dict_list": [{"name": "habibur", "age": 15, "city": "Dhaka"},
-                     {"name": "ali", "age": 25, "city": "Khulna"},
-                     {"name": "hassan", "age": 35, "city": "Jessore"},
-                     ]
-}
-
-
 def about(request):
+    data = {
+        "title": "The quick brown fox jumps over the lazy dog",
+        "name": "habibur",
+        "word": "Django Templates Contex Feature",
+        "sentence": "This is a simple Django Template",
+        "number": "1234567890",
+        "list": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        "my_dict": {"name": "habibur", "age": 25, "city": "Lahore"},
+        "my_html": "<h1><i>Hello World!</i></h1>",
+        "my_date": datetime.now() - timedelta(days=365),
+        "my_string": "alert('hello');",
+        "my_var": None,
+        "is_bangladeshi": True,
+
+        "my_dict_list": [{"name": "habibur", "age": 15, "city": "Dhaka"},
+                         {"name": "ali", "age": 25, "city": "Khulna"},
+                         {"name": "hassan", "age": 35, "city": "Jessore"},
+                         ]
+    }
     context = data.copy()
     context["current_date"] = datetime.now()
     return render(request, 'about.html', context)
